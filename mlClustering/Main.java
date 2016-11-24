f74c148dca90bc107e5b5716a3c9f0e7d56c8f1c
raihannurr <raihannurr@gmail.com>
2016-11-24 14:01:25 +0700

    Update Main.java

--- mlClustering/Main.java
+++ mlClustering/Main.java
@@ -1,25 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package mlClustering;
 
+import weka.clusterers.ClusterEvaluation;
+import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.converters.ConverterUtils;
+import weka.core.converters.ConverterUtils.DataSource;
 
 /**
- * Created by Aidin Ahmad on 24/11/2016.
+ *
+ * @author toshiba
  */
-public class Main {
-    public static void main(String[] args) throws Exception{
-        // TODO code application logic here
+public class MLClustering {
 
-        System.out.println("asdasdasd");
-            System.out.println("tes");
-            ConverterUtils.DataSource source = new ConverterUtils.DataSource("iris.arff");
-            AgnesCluster agnes = new AgnesCluster();
-            MyKMeans kmeans = new MyKMeans(3);
-            Instances datas = source.getDataSet();
-            //System.out.println(datas.a);
-            kmeans.buildClusterer(datas);
-            //agnes.buildClusterer(datas);
-            //System.out.println(agnes.toString());
-            System.out.println("aku");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        DataSource source = new DataSource("iris.arff");
+        AgnesCluster agnes = new AgnesCluster(0);        
+        MyKMeans kmeans = new MyKMeans(3);
+        Instances datas = source.getDataSet();        
+        //System.out.println("* " + datas.size());
+        //System.out.println(datas.a);
+        kmeans.buildClusterer(datas);
+        agnes.buildClusterer(datas);
+        //System.out.println(kmeans.getAllClusters()[0].toString());
+        
+        System.out.println(datas.toSummaryString());
+        for (KMeansCluster cluster : kmeans.getAllClusters()) {
+            System.out.println(cluster.toString());
+        }
+        //agnes.buildClusterer(datas);
+        //ClusterTree<Instance> tes = agnes.getClusterTree();
+        //tes.print("");
+        //ClusterEvaluation eval = new ClusterEvaluation();
+        //eval.setClusterer(kmeans);
+        //eval.evaluateClusterer(datas);
+        //System.out.println(eval.clusterResultsToString());
     }
+    
 }
